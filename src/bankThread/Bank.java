e4915ac05c945300d3c232df17180078eca7aabc
Kyluk Oleg <oleg18kyluk05@gmail.com>
2017-08-26 18:05:46 +0300

    bankThread(add lock)

--- src/bankThread/Bank.java
+++ src/bankThread/Bank.java
@@ -1,13 +1,15 @@
 package bankThread;
 
 import java.util.Arrays;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Bank {
 
     private final double[] accounts;
-
+    private Lock bLock;
+    private Condition requiredAmount;
     public static final int NACCOUNTS = 100;
     public static final int DELAY = 10;
     public static final double INITIAL_BALANCE = 1000;
@@ -17,30 +19,37 @@ public class Bank {
     public Bank(int n, double initialBalance) {
         accounts = new double[n];
         Arrays.fill(accounts, initialBalance);
+        bLock = new ReentrantLock();
+        requiredAmount = bLock.newCondition();
     }
 
-    private Lock bLock = new ReentrantLock();
 
-    public void transfer(int from, int to, double amount) {
-
+    public void transfer(int from, int to, double amount) throws InterruptedException {
         bLock.lock();
         try {
-            if (accounts[from] < amount) return;
+            while (accounts[from] < amount)
+                requiredAmount.await();
             System.out.print(Thread.currentThread());
             accounts[from] -= amount;
             System.out.printf(" %10.2f from %d to %d", amount, from, to);
             accounts[to] += amount;
-            System.out.printf("TBalance: %10.2f%n", getTotalBalance());
+            System.out.printf(" TBalance: %10.2f%n", getTotalBalance());
+            requiredAmount.signalAll();
         } finally {
             bLock.unlock();
         }
     }
 
     public double getTotalBalance() {
-        double sum = 0;
-        for (double a : accounts)
+        bLock.lock();
+        try {
+            double sum = 0;
+            for (double a : accounts)
             sum += a;
-        return sum;
+            return sum;
+        } finally {
+            bLock.unlock();
+        }
 
     }
 
