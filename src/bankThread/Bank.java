7a9da129b8da2a1e995688e8ad5721a6b6295d97
Kyluk Oleg <oleg18kyluk05@gmail.com>
2017-08-26 16:10:32 +0300

    bankThread

--- src/bankThread/Bank.java
+++ src/bankThread/Bank.java
@@ -1,4 +1,41 @@
 package bankThread;
 
+import java.util.Arrays;
+
 public class Bank {
+
+    private final double[] accounts;
+
+    public static final int NACCOUNTS = 100;
+    public static final int DELAY = 10;
+    public static final double INITIAL_BALANCE = 1000;
+    public static final double MAX_AMOUNT = 1000;
+
+
+    public Bank(int n, double initialBalance) {
+        accounts = new double[n];
+        Arrays.fill(accounts, initialBalance);
+    }
+
+    public void transfer(int from, int to, double amount) {
+        if (accounts[from] < amount) return;
+        System.out.print(Thread.currentThread());
+        accounts[from] -= amount;
+        System.out.printf(" %10.2f from %d to %d", amount, from, to);
+        accounts[to] += amount;
+        System.out.printf("TBalance: %10.2f%n", getTotalBalance());
+    }
+
+    public double getTotalBalance() {
+        double sum = 0;
+        for (double a : accounts)
+            sum += a;
+        return sum;
+
+    }
+
+    public int size() {
+        return accounts.length;
+    }
+
 }
--- src/bankThread/BankTest.java
+++ src/bankThread/BankTest.java
@@ -2,6 +2,23 @@ package bankThread;
 
 public class BankTest {
     public static void main(String[] args) {
-
+        Bank bank = new Bank(Bank.NACCOUNTS, Bank.INITIAL_BALANCE);
+        for (int i = 0; i < Bank.NACCOUNTS; i++) {
+            int fromAccount = i;
+            Runnable r = () -> {
+                try {
+                    while (true) {
+                        int toAccount = (int) (bank.size() * Math.random());
+                        double amount = Bank.MAX_AMOUNT * Math.random();
+                        bank.transfer(fromAccount, toAccount, amount);
+                        Thread.sleep((int) (Bank.DELAY * Math.random()));
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            };
+            Thread t = new Thread(r);
+            t.start();
+        }
     }
 }
