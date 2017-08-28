f49545a17821c38549dec7d089959bf8569cedaf
Kyluk Oleg <oleg18kyluk05@gmail.com>
2017-08-28 18:41:48 +0300

    bankThread(synchronized)

---
+++ src/blockQ/BlockingQTest.java
@@ -0,0 +1,81 @@
+package blockQ;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class BlockingQTest {
+
+    private static final int FILE_QUEUE_SIZE = 10;
+    private static final int SEARCH_THREADS = 100;
+    private static final File FILE = new File("");
+    private static BlockingQueue<File> queue = new ArrayBlockingQueue<File>(FILE_QUEUE_SIZE);
+
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print("enter directory: ");
+            String directory = in.nextLine();
+            System.out.print("enter keyword: ");
+            String keyword = in.nextLine();
+
+            Runnable enumerator = () -> {
+                try {
+                    enumerate(new File(directory));
+                    queue.put(FILE);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            };
+
+            new Thread(enumerator).start();
+            ;
+            for (int i = 1; i <= SEARCH_THREADS; i++) {
+                Runnable searcher = () -> {
+                    try {
+                        boolean done = false;
+                        while (!done) {
+                            File file = queue.take();
+                            if (file == FILE) {
+                                queue.put(file);
+                                done = true;
+                            } else search(file, keyword);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                };
+                new Thread(searcher).start();
+            }
+
+        }
+    }
+
+    public static void enumerate(File directory) throws InterruptedException {
+
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) enumerate(file);
+            else queue.put(file);
+        }
+
+    }
+
+    public static void search(File file, String keyword) throws IOException {
+
+        try (Scanner in = new Scanner(file, "UTF-8")) {
+            int lineNumber = 0;
+            while (in.hasNextLine()) {
+                lineNumber++;
+                String line = in.nextLine();
+                if (line.contains(keyword))
+                    System.out.printf("%s:%d:%s%n", file.getPath(), lineNumber, line);
+            }
+
+        }
+    }
+}
